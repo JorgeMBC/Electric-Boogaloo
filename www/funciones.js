37ed582228096b8556247e943f9e77096ae12045
JorgeMBC <jorgedg53@gmail.com>
2015-04-10 18:45:23 -0500

    gestos tactiles
    funciones de swipe

---
+++ www/funciones.js
@@ -0,0 +1,14 @@
+$(document).on('pageinit', function(event){
+	$('div.ui-page').on("swipeleft", function(){
+		var nextpage = $(this).next('div[data-role="page"]');
+			if (nextpage.length > 0){
+				$.mobile.changePage(nextpage, "slide", false, true);
+				}
+	});
+	$('div.ui-page').on("swiperight", function () {
+		var prevpage = $(this).prev('div[data-role="page"]');
+		if (prevpage.length > 0) {
+			$.mobile.changePage(prevpage, {transition: "slide", reverse: true}, true, true);
+		}
+	});
+});
